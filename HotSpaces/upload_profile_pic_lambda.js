8a8f15a8413aa1b0f7a9e8a791719dc6d869c38f
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-24 11:49:34 +0530

    Adding upload-user-profile

--- HotSpaces/upload_profile_pic_lambda.js
+++ HotSpaces/upload_profile_pic_lambda.js
@@ -1,6 +1,31 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const s3 = new AWS.S3();
+exports.handler = function (event, context, callback) {
+ 	let encodedImage =JSON.parse(event.body).user_avatar;
+     let decodedImage = Buffer.from(encodedImage, 'base64');
+	  var filePath = "hotspaces-avatars/" + event.queryStringParameters.username + ".jpg";
 
+	s3.putObject({
+		"Body":  decodedImage,
+		"Bucket": "find-my-mate-hasangi",
+		"Key": filePath 
+	})
+		.promise()
+		.then(data => {
+			console.log(data);           // successful response
+		let response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(data),
+        "isBase64Encoded": false
+    };
+           callback(null, response);
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+			callback(err, null);
+		});
 
-	callback(null,'Successfully executed');
 }
