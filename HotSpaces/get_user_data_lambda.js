541524e0ebfaff3c9247dc060bcc47c4ad49a866
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-24 11:42:16 +0530

    Adding Get-User

--- HotSpaces/get_user_data_lambda.js
+++ HotSpaces/get_user_data_lambda.js
@@ -1,6 +1,25 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.get({
+		TableName: 'hs_user',
+		Key: { 'username': event.queryStringParameters.username }
+	}, function (err, data) {
+		if (err) {
+			  callback(err, null);
+		} else {
+			 let response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(data),
+        "isBase64Encoded": false
+    };
+           callback(null, response);
+		}
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
