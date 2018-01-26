d94d71fe57149e5ade5de8eb205fa488f6e064b8
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-26 10:58:33 +0530

    updating saving device token implementation

--- HotSpaces/update_device_token.js
+++ HotSpaces/update_device_token.js
@@ -5,12 +5,12 @@ exports.handler = function (event, context, callback) {
 	console.log(event.body);
 	console.log(JSON.parse(event.body).username);
 	ddb.put({
-		TableName: 'hs_device_tokens',
+		TableName: 'device_token',
 		Item: {
-			'username': JSON.parse(event.body).username,
+			'username': JSON.parse(event.body).username},
 			'device_token': JSON.parse(event.body).token
 		}
-	}, function (err, data) {
+	, function (err, data) {
 		if (err) {
 			callback(err, null);
 		} else {
