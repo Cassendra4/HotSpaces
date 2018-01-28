537147482a7e3789b534892c20471fcb51609f4c
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-28 22:06:14 +0530

    adding create user

--- HotSpaces/create_user_lambda.js
+++ HotSpaces/create_user_lambda.js
@@ -3,25 +3,57 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	ddb.put({
 		TableName: 'hs_user',
-		Item: { 'username': '', 'email': '', 'first_name': '', 'last_name': '', 'age': 'NaN', 'gender': 'NaN', 'interested_in': 'NaN', 'password': '', 'user_avatar': '', 'ARNs': '', 'contact_number': 'NaN' }
+		Item: {
+			'username': event.username,
+			'email': event.email,
+			'first_name': event.first_name,
+			'last_name': event.last_name,
+			'age': event.age,
+			'gender': event.gender,
+			'interested_in': event.interested_in,
+			'password': event.password,
+			'user_avatar': event.user_avatar,
+			'contact_number': event.contact_number
+		}
 	}, function (err, data) {
 		if (err) {
-			//handle error
+			callback(err, null);
 		} else {
-			//your logic goes here
+			let response = {
+				"statusCode": 200,
+				"headers": {
+					"my_header": "my_value"
+				},
+				"body": JSON.stringify(data),
+				"isBase64Encoded": false
+			};
+			callback(null, response);
 		}
 	});
 	ddb.put({
 		TableName: 'hs_sort_table',
-		Item: { 'hask_key': '', 'last_known_lat': '', 'last_known_long': '', 'last_updated_timestamp': '', 'gender': 'NaN', 'username': '' }
+		Item: {
+			'gender': event.gender,
+			'last_updated_timestamp': event.last_updated_timestamp,
+			'last_known_lat': event.last_known_lat,
+			'last_known_long': event.last_known_long,
+			'username': event.username
+		}
 	}, function (err, data) {
 		if (err) {
-			//handle error
+			callback(err, null);
 		} else {
-			//your logic goes here
+			let response = {
+				"statusCode": 200,
+				"headers": {
+					"my_header": "my_value"
+				},
+				"body": JSON.stringify(data),
+				"isBase64Encoded": false
+			};
+			callback(null, response);
 		}
 	});
 
 
-	callback(null, 'Successfully executed');
 }
