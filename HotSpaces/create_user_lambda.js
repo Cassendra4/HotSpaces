6155988cf5e9cdbba59ed9d1c75806bb9bcb776e
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-28 23:16:21 +0530

    adding create user

--- HotSpaces/create_user_lambda.js
+++ HotSpaces/create_user_lambda.js
@@ -1,22 +1,20 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-	console.log(event);
-	console.log(event.body);
-	console.log(JSON.parse(event.body).username);
+	
 	ddb.put({
 		TableName: 'hs_user',
 		Item: {
-			'username': event.username,
-			'email': event.email,
-			'first_name': event.first_name,
-			'last_name': event.last_name,
-			'age': event.age,
-			'gender': event.gender,
-			'interested_in': event.interested_in,
-			'password': event.password,
-			'user_avatar': event.user_avatar,
-			'contact_number': event.contact_number
+			'username': JSON.parse(event.body).username,
+			'email': JSON.parse(event.body).email,
+			'first_name':JSON.parse(event.body).first_name,
+			'last_name': JSON.parse(event.body).last_name,
+			'age': JSON.parse(event.body).age,
+			'gender': JSON.parse(event.body).gender,
+			'interested_in': JSON.parse(event.body).interested_in,
+			'password': JSON.parse(event.body).password,
+			'user_avatar': JSON.parse(event.body).user_avatar,
+			'contact_number': JSON.parse(event.body).contact_number
 		}
 	}, function (err, data) {
 		if (err) {
@@ -36,11 +34,11 @@ exports.handler = function (event, context, callback) {
 	ddb.put({
 		TableName: 'hs_sort_table',
 		Item: {
-			'gender': event.gender,
-			'last_updated_timestamp': event.last_updated_timestamp,
-			'last_known_lat': event.last_known_lat,
-			'last_known_long': event.last_known_long,
-			'username': event.username
+			'gender': JSON.parse(event.body).gender,
+			'last_updated_timestamp': JSON.parse(event.body).last_updated_timestamp,
+			'last_known_lat': JSON.parse(event.body).last_known_lat,
+			'last_known_long': JSON.parse(event.body).last_known_long,
+			'username': JSON.parse(event.body).username
 		}
 	}, function (err, data) {
 		if (err) {
