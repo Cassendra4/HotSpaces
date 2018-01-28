c0ab539949291b02941838ae6b8142b6bb4b74de
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-28 23:08:27 +0530

    adding create user

--- HotSpaces/create_user_lambda.js
+++ HotSpaces/create_user_lambda.js
@@ -3,7 +3,7 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	console.log(event);
 	console.log(event.body);
-	console.log(event.username);
+	console.log(event.body.username);
 	ddb.put({
 		TableName: 'hs_user',
 		Item: {
