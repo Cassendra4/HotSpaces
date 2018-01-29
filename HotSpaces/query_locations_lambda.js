c8e3ecb74bbbdf63dccf2801ac4c510df7c7b9cc
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-29 18:30:22 +0530

    updating query locations

--- HotSpaces/query_locations_lambda.js
+++ HotSpaces/query_locations_lambda.js
@@ -2,24 +2,6 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
-	
-	ddb.query({
-		TableName: 'hs_sort_table',
-		ExpressionAttributeValues: {
-			':gender': 'NaN',
-			':timestamp': '',
-			':lat': '',
-			':long': ''
-		},
-		KeyConditionExpression: 'gender = :gender and last_updated_timestamp >= :timestamp',
-		FilterExpression: 'last_known_lat >= :lat and last_known_long >= :long'
-	}, function (err, data) {
-		if (err) {
-			//handle error
-		} else {
-			//your logic goes here
-		}
-	});
 
 
 }
