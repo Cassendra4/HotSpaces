6cbc348c95d7610932c4c455355fbfdab4089abc
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-29 12:38:29 +0530

    updating create user

--- HotSpaces/get_user_data_lambda.js
+++ HotSpaces/get_user_data_lambda.js
@@ -1,9 +1,8 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = function (event, context, callback) {
-	console.log(event);
-	console.log(event.queryStringParameters);
-	console.log(event.queryStringParameters.username);
+	
 	ddb.get({
 		TableName: 'hs_user',
 		Key: { 'username': event.queryStringParameters.username }
---
+++ HotSpaces/update_user_lambda.js
@@ -0,0 +1,6 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+
+}
