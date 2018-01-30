8bd7659b697d6eef86315d441812fccbfad6626c
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-30 10:54:33 +0530

    updating get request from

--- HotSpaces/send_reveal_request_lambda.js
+++ HotSpaces/send_reveal_request_lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
@@ -34,21 +35,47 @@ exports.handler = function (event, context, callback) {
 				if (err) {
 					callback(err, null);
 				} else {
-					
-					data.Items.forEach(function(item) {
-               
 
+					data.Items.forEach(function (item) {
+						sns.createPlatformEndpoint({
+							PlatformApplicationArn: 'arn:aws:sns:us-east-1:480964559519:app/GCM/hotspaces',
+							Token: item.device_token
+						}).promise()
+							.then(data => {
+								callback(err, null);
+							})
+							.catch(err => {
+								console.log(data.EndpointArn);
+								endPointData = data.EndpointArn;
 
-					 });
+								sns.publish({
+									Message: JSON.stringify(jsonObj),
+									Subject: 'Reveal Request',
+									TargetArn: endPointData
+								}).promise()
+									.then(data => {
+										callback(err, null);
+									})
+									.catch(err => {
+									 let response = {
+											"statusCode": 200,
+											"headers": {
+												"my_header": "my_value"
+											},
+											"body": JSON.stringify(data),
+											"isBase64Encoded": false
+										};
+											callback(null, response);
+									});
+
+							});
+
+					});
 
 				}
 			});
 
-
-
 		}
 	});
 
-
-	callback(null, 'Successfully executed');
 }
