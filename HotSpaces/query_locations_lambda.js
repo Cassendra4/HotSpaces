df1da7a2dd48bc8152cb5382e868c3c8bf15ccf1
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-01-30 11:25:29 +0530

    updating query locations

--- HotSpaces/query_locations_lambda.js
+++ HotSpaces/query_locations_lambda.js
@@ -2,19 +2,63 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
   var currentTimestamp = new Date();
-    var yesterdayTimestamp = new Date();
-    yesterdayTimestamp.setTime(currentTimestamp.getTime() - 86400000);
+  var yesterdayTimestamp = new Date();
+  yesterdayTimestamp.setTime(currentTimestamp.getTime() - 86400000);
 
-    let dbParams;
+  let dbParams = 1;
+
+  if (parseInt(event.queryStringParameters.gender) != 2) {
+    ddb.query({
+      TableName: 'hs_sort_table',
+      ExpressionAttributeValues: { ':gender': parseInt(event.queryStringParameters.gender), ':yesterdayTimestamp': yesterdayTimestamp.toLocaleString() },
+      KeyConditionExpression: 'gender = :gender',
+      FilterExpression: 'last_updated_timestamp > :yesterdayTimestamp',
+    }, function (err, data) {
+      if (err) {
+         callback(err, null);
+      } else {
+          let response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(data),
+        "isBase64Encoded": false
+    };
+           callback(null, response);
+      }
+    });
+
+  } else {
+
+ dbParams = {
+        TableName: 'hs_sort_table',
+		ExpressionAttributeValues: {
+			':yesterdayTimestamp': yesterdayTimestamp.toLocaleString()
+		},
+	FilterExpression:  'last_updated_timestamp > :yesterdayTimestamp'
+    };
     
-    if (parseInt(event.queryStringParameters.gender) != 2) {
-        
-        
-        
-          } else {
+      docClient.scan(dbParams, function(err, data) {
+       console.log("err", err);
+       console.log("data", data);
+       if(err) {
+           callback(err, null);
+       } else {
+           
+            let response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(data),
+        "isBase64Encoded": false
+    };
+           callback(null, response);
+       }
+    });
 
-
-    }
+  }
 
 
 }
