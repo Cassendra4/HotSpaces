c2e6083eafb619c8897173d3083c1d9ee2e1ef2a
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-02-07 15:48:19 +0530

    updating create user

--- HotSpaces/create_user_lambda.js
+++ HotSpaces/create_user_lambda.js
@@ -1,20 +1,33 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
- var currentTimestamp = new Date();
+	var currentTimestamp = new Date();
+	var username = JSON.parse(event.body).username;
+	var email = JSON.parse(event.body).email;
+	var firstName = JSON.parse(event.body).first_name;
+	var lastName = JSON.parse(event.body).last_name;
+	var age = JSON.parse(event.body).age;
+	var gender = JSON.parse(event.body).gender;
+	var interestedIn = JSON.parse(event.body).interested_in;
+	var password = JSON.parse(event.body).password;
+	var userAvatar = JSON.parse(event.body).user_avatar;
+	var contactNumber = JSON.parse(event.body).contact_number;
+	var lat = JSON.parse(event.body).last_known_lat;
+	var long = JSON.parse(event.body).last_known_long; 
+	var currentTime = currentTimestamp.toLocaleString();
 	ddb.put({
 		TableName: 'hs_user',
 		Item: {
-			'username': JSON.parse(event.body).username,
-			'email': JSON.parse(event.body).email,
-			'first_name': JSON.parse(event.body).first_name,
-			'last_name': JSON.parse(event.body).last_name,
-			'age': JSON.parse(event.body).age,
-			'gender': JSON.parse(event.body).gender,
-			'interested_in': JSON.parse(event.body).interested_in,
-			'password': JSON.parse(event.body).password,
-			'user_avatar': JSON.parse(event.body).user_avatar,
-			'contact_number': JSON.parse(event.body).contact_number
+			'username': username,
+			'email': email,
+			'first_name': firstName,
+			'last_name': lastName,
+			'age': age,
+			'gender': gender,
+			'interested_in': interestedIn,
+			'password': password,
+			'user_avatar': userAvatar,
+			'contact_number': contactNumber
 		}
 	}, function (err, data) {
 		if (err) {
@@ -23,13 +36,13 @@ exports.handler = function (event, context, callback) {
 
 			ddb.put({
 				TableName: 'hs_sort_table',
-				Item: { 
-				'gender': JSON.parse(event.body).gender, 
-				'username': JSON.parse(event.body).username, 
-				'last_known_lat': JSON.parse(event.body).last_known_lat, 
-				'last_updated_timestamp':  currentTimestamp.toLocaleString(), 
-				'last_known_long': JSON.parse(event.body).last_known_long
-				 }
+				Item: {
+					'gender': gender,
+					'username': username,
+					'last_known_lat': lat,
+					'last_updated_timestamp': currentTime,
+					'last_known_long': long
+				}
 			}, function (err, data) {
 				if (err) {
 					callback(err, null);
